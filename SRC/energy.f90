b9e1410534f80eab6bb5fcdfee70feaf05cb93e7
Mark Szepieniec <mark.szepieniec@tyndall.ie>
2012-07-04 12:28:08 +0100

    fixed computation of energy expectation value in energy.f90

--- SRC/energy.f90
+++ SRC/energy.f90
@@ -14,15 +14,18 @@ subroutine energy(length,eval,dnorm)
   if(ijs(1) .ne. length + 2) STOP 'energy: s array mismatch'
 
   eval  = (0.0_pr, 0.0_pr)
-  dnorm = 0.0_pr
+  dnorm = (0.0_pr, 0.0_pr)
   do ici=1, length
-     eval  = eval  + c(ici)*h(ici)*c(ici)
-     dnorm = dnorm + c(ici)*s(ici)*c(ici)
+     eval  = eval  + conjg(c(ici))*h(ici)*c(ici)
+     dnorm = dnorm + conjg(c(ici))*s(ici)*c(ici)
      do jci = ijh(ici), ijh(ici+1) - 1
-        eval  =  eval  + 2.0*c(ici)*h(jci)*c(ijh(jci))
+        !eval  =  eval  + 2.0*real(conjg(c(ici))*h(jci)*c(ijh(jci)), kind=pr)
+        eval  =  eval  + conjg(c(ici))*h(jci)*c(ijh(jci))  &
+                       + c(ici)*h(jci)*conjg(c(ijh(jci)))
      enddo
      do jci = ijs(ici), ijs(ici+1) - 1
-        dnorm =  dnorm + 2.0*c(ici)*s(jci)*c(ijs(jci))
+        dnorm =  dnorm + conjg(c(ici))*s(jci)*c(ijs(jci))  &
+                       + c(ici)*s(jci)*conjg(c(ijs(jci)))
      enddo
   enddo
 
